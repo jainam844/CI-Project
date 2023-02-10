9c99d44c8f9ffe184bcca53cea01be2f4c6caed8
Jainam Shah <PCA158@server7.com>
2023-02-10 18:13:33 +0530

    mvc  commit

---
+++ MVC/CI PLatform/CI PLatform_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CI_PLatform_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
